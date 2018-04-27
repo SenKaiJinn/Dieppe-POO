8a044da65358c51e8d07c0b994aed836ef687e71
lannel <jean.lannel@hotmail.com>
2018-04-27 09:39:09 +0200

    Ajout des fonctions constructeur

---
+++ Querie.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace tools;
+
+class Querie
+{
+    private $dsn = "mysql:dbname=easywedding;host=localhost;charset=utf8";
+    private $user ="root";
+    private $password = "";
+    private $db;
+
+        public function __construct()
+    {
+        try
+        {
+            $this->db = new PDO($this->dsn, $this->user, $this->password);
+            $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+        }
+
+        catch (PDOExecption $e)
+        {
+            Log::logwrite($e->getMessage());
+        }
+
+    }
+
+        public function selectMethod($sql)
+    {
+
+    }
+    }
+    }
+    public function selectMethod($sql)
+ {
+
+ }
+ public function __destruct()
+{
+    unset($this->db);
+}
